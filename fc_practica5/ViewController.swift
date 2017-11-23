43db3786dc2857b2e761fd631b99c50fae14c586
carlosvillasanchez <carlos.villa.vs@gmail.com>
2017-11-23 10:20:25 +0100

    Initial Commit

---
+++ fc_practica5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fc_practica5
+//
+//  Created by Carlos Villa on 23/11/2017.
+//  Copyright © 2017 Carlos Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
